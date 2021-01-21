0c47f3eef79505da21278b1fe82234cfbef51c6e
racross1 <racross1@gmail.com>
2021-01-21 15:36:08 -0800

    Done.

--- index.js
+++ index.js
@@ -13,30 +13,11 @@ function fuzzyMatch(arr, str) {
     })     
 }
 
-
-const drivers = [
-    {
-      name: 'Bobby',
-      hometown: 'Pittsburgh' },
-    {
-      name: 'Sammy',
-      hometown: 'New York' } ,
-    {
-      name: 'Sally',
-      hometown: 'Cleveland' },
-    {
-      name: 'Annette',
-      hometown: 'Los Angeles' },
-    {
-      name: 'Bobby',
-      hometown: 'Tampa Bay' }
-  ];
-
 function matchName(arr, str) {
     return arr.filter(obj => {
         if (obj['name'].toUpperCase() === str.toUpperCase()) {
             return obj
         }        
     })
-    
+}
     
