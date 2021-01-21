d24043f4017df92f4a8fa6f125f50aaa23f663a2
racross1 <racross1@gmail.com>
2021-01-21 15:35:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution here
+const { objectContaining } = require("expect")
+
+function findMatching(arr, str){
+    return arr.filter(n => {
+        return n.toUpperCase() === str.toUpperCase()
+    }) 
+}
+
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(n => {
+        return n[0].toUpperCase() === str[0].toUpperCase()
+    })     
+}
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(arr, str) {
+    return arr.filter(obj => {
+        if (obj['name'].toUpperCase() === str.toUpperCase()) {
+            return obj
+        }        
+    })
+    
+    
